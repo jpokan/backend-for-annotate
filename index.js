f51e712a12b87607b369856739b32c99407029a2
Jorge Ou <8276144+Topomon@users.noreply.github.com>
2021-10-10 14:21:26 -0500

    Router for Notion Auth routes

--- index.js
+++ index.js
@@ -20,66 +20,9 @@ app.get('/', (req, res) => {
 	res.sendFile(`${__dirname}/static/index.html`)
 })
 
-const getAccessToken = require('./auth/notion/get-access-token')
-const frontendURL = process.env.FRONTEND_URL
-
-/**
- * Notion Public Integration Routes
- */
-
-app.get('/auth/notion', async (req, res) => {
-	try {
-		const code = req.query.code
-		const result = await getAccessToken(code)
-		if (result) {
-			// Delete stored session if it exists
-			// Save results to session
-			req.session.token = result
-		}
-		res.redirect(frontendURL)
-	} catch (error) {
-		res.redirect(frontendURL)
-	}
-})
-
-app.get('/auth/notion/session/status', async (req, res) => {
-	res.set('Access-Control-Allow-Origin', frontendURL)
-	res.set('Access-Control-Allow-Credentials', true)
-	res.set('Access-Control-Allow-Headers', 'Accept')
-
-	// if session with access token exist
-	// it is an authenticated user
-	if (req.session.token) {
-		// Use token to get data from Notion API
-		const response = {
-			id: req.sessionID,
-			online: true
-		}
-		res.json(response)
-		// else the session is not available and does not have any data
-	} else {
-		res.status(403).json({
-			id: req.sessionID,
-			online: false,
-			error_message: 'No connection to Notion has been set'
-		})
-	}
-})
-
-app.post('/auth/notion/session/disconnect', async (req, res) => {
-	res.set('Access-Control-Allow-Origin', frontendURL)
-	res.set('Access-Control-Allow-Credentials', true)
-	res.set('Access-Control-Allow-Headers', 'Accept')
-
-	req.session.cookie.maxAge = 1
-	req.session.touch()
-	const response = {
-		id: req.sessionID,
-		online: false
-	}
-	console.log('Session was terminated.')
-	res.json(response)
-})
+// Notion Auth routes
+const notionAuth = require('./auth/notion/routes')
+app.use('/auth/notion', notionAuth)
 
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log('listenning to port: ' + port))
