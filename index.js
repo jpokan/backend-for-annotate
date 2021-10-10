0e963674c8142d4b36dd1ba12be7211cc3484b6c
Jorge Ou <8276144+Topomon@users.noreply.github.com>
2021-10-10 14:09:45 -0500

    re using dotenv in production

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
+require('dotenv').config()
 const app = require('express')()
 
 console.log('Starting server in: ' + app.get('env') + ' mode')
-
-if (app.get('env') === 'development') {
-	require('dotenv').config()
-} else {
-	// trust first proxy is required if server is behind a proxy server
-	app.set('trust proxy', 1) // true in production
-}
 
 const session = require('express-session')
 const session_config = require('./sessions/config')
 
 // GLOBAL PRODUCTION SETTINGS
 if (process.env.NODE_ENV === 'production') {
+	// trust first proxy is required if server is behind a proxy server
+	app.set('trust proxy', 1) // true in production
 	// Cookies in production needs to be sent with these options
 	session_config.cookie.sameSite = 'none' // Because cookies are sent to a different domains or site
 	session_config.cookie.secure = true // Because cookies must be sent with HTTPS
